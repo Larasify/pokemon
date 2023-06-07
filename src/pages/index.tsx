bac6cdc8e9be0a822228d9795214f15d5433859c
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-07 13:33:34 +0300

    random pokemon id fetcher with state for hydration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
+import { useEffect, useState } from "react";
+import { getOptionsForVote } from "../utils/getRandomPokemonHelper";
 import { trpc } from "../utils/trpc";
 
 export default function Home() {
-  const {data, isLoading} = trpc.hello.useQuery({ text: "client" });
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
+  const [[first, second], setOptions] = useState([0, 0]);
+
+  useEffect(() => {
+    const [firstId, seconId] = getOptionsForVote();
+    setOptions([firstId, seconId]);
+  }, []);
+
   return (
     //center the div
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-400 text-center">First</div>
+        <div className="w-16 h-16 bg-red-400 text-center">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-400">{data?.greeting}</div>
+        <div className="w-16 h-16 bg-red-400">{second}</div>
       </div>
     </div>
   );
