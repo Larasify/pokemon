2c7969a0b9aca26333a9fcdca8401686f6e9f2a4
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-07 10:28:38 +0300

    very basic ui template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
-
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline text-red-200">Hello world!</h1>;
+  return (
+    //center the div
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which pokemon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-400 flex-col justify-center flex text-center">
+          First
+        </div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-400">Second</div>
+      </div>
+    </div>
+  );
 }
