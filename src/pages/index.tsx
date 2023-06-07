d6fc22d56bad7430c934f1a9566e2bb2c4368d2a
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-07 10:54:29 +0300

    add tRpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
+import { trpc } from "../utils/trpc";
+
 export default function Home() {
+  const {data, isLoading} = trpc.hello.useQuery({ text: "client" });
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
   return (
     //center the div
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-400 text-center">
-          First
-        </div>
+        <div className="w-16 h-16 bg-red-400 text-center">First</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-400">Second</div>
+        <div className="w-16 h-16 bg-red-400">{data?.greeting}</div>
       </div>
     </div>
   );
