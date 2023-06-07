54767bb13aeb5a545c3450d017bdb58ecff814d9
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-07 17:20:31 +0300

    add prisma and voting api

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,29 +1,42 @@
-import { z } from 'zod';
-import { procedure, router } from '../trpc';
-import { PokemonClient } from 'pokenode-ts';
-import { TRPCError } from '@trpc/server';
+import { z } from "zod";
+import { procedure, router } from "../trpc";
+import { PokemonClient } from "pokenode-ts";
+import { TRPCError } from "@trpc/server";
+import { prisma } from "../utils/prisma";
+
 
 export const appRouter = router({
   hello: procedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
       return {
         greeting: `hello ${opts.input.text}`,
       };
     }),
-    
-    getpokemonbyid: procedure.input(z.object({id: z.number()})).query(async ({input}) => {
+
+  getpokemonbyid: procedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ input }) => {
       const api = new PokemonClient();
       const pokemonid = input.id;
       if (pokemonid < 1 || pokemonid > 898) return null;
-      const pokemon =  await api.getPokemonById(pokemonid);
-      return {name: pokemon.name, sprite: pokemon.sprites.front_default};
+      const pokemon = await api.getPokemonById(pokemonid);
+      return { name: pokemon.name, sprite: pokemon.sprites.front_default };
     }),
 
+  castvote: procedure.input(z.object({votedFor:z.number(), votedAgainst:z.number()})).mutation(async ({input}) => {
+    const voteInDb = await prisma.vote.create({
+      data:{
+        votedFor: input.votedFor,
+        votedAgainst: input.votedAgainst
+      }
+    })
+    return {success: true, vote: voteInDb};
+  }),
 });
 
 // export type definition of API
