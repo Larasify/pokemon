11c5b2db1bcd4e70e8b922cd8a9aaa5a54fbe81b
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-07 14:19:33 +0300

    pokemon api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,15 +10,48 @@ export default function Home() {
     setOptions([firstId, seconId]);
   }, []);
 
+  const FirstPokemon = trpc.getpokemonbyid.useQuery(
+    { id: first },
+    { staleTime: Infinity }
+  );
+  const SecondPokemon = trpc.getpokemonbyid.useQuery(
+    { id: second },
+    { staleTime: Infinity }
+  );
+
+  if (FirstPokemon.isLoading || SecondPokemon.isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!FirstPokemon.data || !SecondPokemon.data) {
+    return <div>Something went wrong</div>;
+  }
   return (
     //center the div
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-400 text-center">{first}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            src={FirstPokemon.data.pokemon.sprites.front_default!}
+            className="w-full"
+            alt=""
+          />
+          <div className="text-center text-xl capitalize mt-[-2rem]">
+            {FirstPokemon.data.pokemon.name}
+          </div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-400">{second}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            src={SecondPokemon.data.pokemon.sprites.front_default!}
+            className="w-full"
+            alt=""
+          />
+          <div className="text-center text-xl capitalize mt-[-2rem]">
+            {SecondPokemon.data.pokemon.name}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { procedure, router } from '../trpc';
+import { PokemonClient } from 'pokenode-ts';
 
 export const appRouter = router({
   hello: procedure
@@ -13,6 +14,15 @@ export const appRouter = router({
         greeting: `hello ${opts.input.text}`,
       };
     }),
+    
+    getpokemonbyid: procedure.input(z.object({id: z.number()})).query(async ({input}) => {
+      const api = new PokemonClient();
+      const pokemonid = input.id;
+      const pokemon =  await api.getPokemonById(pokemonid);
+      console.log(pokemon.name + " " + pokemon.id + " " + pokemon.sprites.front_default);
+      return {pokemon};
+    }),
+
 });
 
 // export type definition of API
