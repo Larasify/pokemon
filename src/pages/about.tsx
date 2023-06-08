0c1d93b8f28ea38093b1a13fc453dc48e22a53bb
Larasify <lara.cikis@gmail.com>
2023-06-08 09:37:10 +0300

    fix about padding

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -30,7 +30,7 @@ export default function AboutPage() {
           </div>
         </div>
       </div>
-      <div className="w-full text-center text-xl pb-2">
+      <div className="w-full text-center text-xl pb-4">
         <a href="https://github.com/Larasify/pokemon">Github</a>
         <span className="p-4">{"-"}</span>
         <Link href="/results">Results</Link>
